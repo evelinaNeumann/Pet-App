8fcdd1ed1ce483fef1d2d8ce9510d64f02269d4a
Barbara Ramirez <brcmua@gmail.com>
2023-07-09 18:55:53 +0200

    owner signup styling

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -48,40 +48,40 @@ function LoginPage() {
   return (
     <div className="LoginPage">
       <div className="min-h-screen py-40" style={{ backgroundImage: "linear-gradient(115deg, rgb(237, 135, 51), rgb(255, 175, 128))" }}>
-      <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
-          <div className="w-full lg:w-1/2 flex flex-col items-center p-12 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${LoginImg})`, objectFit: "scale-down" }}>
-          </div>
-          <div className="w-full lg:w-1/2 py-16 px-12">
-            <h2  className="text-3xl mb-4">Login</h2>
-            <p class="mb-4">
-              Welcome back!
-            </p>
-            <form onSubmit={handleLoginSubmit}>
-              <div className="mt-5">
-                <input className="border border-gray-400 py-1 px-2 w-full" type="email" name="email" value={email} onChange={handleEmail} placeholder="Enter you email"/>
-              </div>
-              <div className="mt-5">
-                <input
-                type="password"
-                name="password"
-                value={password}
-                onChange={handlePassword}
-                className="border border-gray-400 py-1 px-2 w-full"
-                placeholder="********"
-                />
-              </div>
-              <div className="mt-5">
-                <button className="w-full py-3 text-center text-white" style={{ backgroundColor: "rgb(63, 98, 18)" }}  type="submit">Login</button>
-              </div>
-            </form>
-            {errorMessage && <p className="error-message">{errorMessage}</p>}
-            <p>Don't have an account yet?</p>
-            <Link style={{color: "rgb(63, 98, 18)"}} className="font-bold" to={"/signup"}> Sign Up</Link>
+        <div className="container mx-auto">
+          <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
+            <div className="w-full lg:w-1/2 flex flex-col items-center p-12 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${LoginImg})`, objectFit: "scale-down" }}>
+            </div>
+            <div className="w-full lg:w-1/2 py-16 px-12">
+              <h2 className="text-3xl mb-4">Login</h2>
+              <p class="mb-4">
+                Welcome back!
+              </p>
+              <form onSubmit={handleLoginSubmit}>
+                <div className="mt-5">
+                  <input className="border border-gray-400 py-1 px-2 w-full" type="email" name="email" value={email} onChange={handleEmail} placeholder="Enter you email" />
+                </div>
+                <div className="mt-5">
+                  <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handlePassword}
+                    className="border border-gray-400 py-1 px-2 w-full"
+                    placeholder="********"
+                  />
+                </div>
+                <div className="mt-5">
+                  <button className="w-full py-3 text-center text-white" style={{ backgroundColor: "rgb(63, 98, 18)" }} type="submit">Login</button>
+                </div>
+              </form>
+              {errorMessage && <p className="error-message">{errorMessage}</p>}
+              <p>Don't have an account yet?</p>
+              <Link style={{ color: "rgb(63, 98, 18)" }} className="font-bold" to={"/signup"}> Sign Up</Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
               {/* <form onSubmit={handleLoginSubmit}>
         <label>Email:</label>
         <input type="email" name="email" value={email} onChange={handleEmail} />
--- client/src/pages/OwnerSignupPage/OwnerSignupPage.jsx
+++ client/src/pages/OwnerSignupPage/OwnerSignupPage.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import authService from "../../services/auth.service";
+import PetImg2 from "../../images/petsimage.png"
 // import axios from "axios";
 
 function OwnerSignupPage() {
   const [ownerEmail, setOwnerEmail] = useState("");
   const [ownerPassword, setOwnerPassword] = useState("");
   const [ownerName, setOwnerName] = useState("");
-  const [ownerPhone, setOwnerPhone] = useState(0);
+  const [ownerPhone, setOwnerPhone] = useState();
   const [city, setCity] = useState("");
   const [state, setState] = useState("");
-  const [zip, setZip] = useState(0);
+  const [zip, setZip] = useState();
   const [country, setCountry] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
 
@@ -45,40 +46,67 @@ function OwnerSignupPage() {
   };
 
   return (
-    <div className="SignupPage">
-      <h1>Sign Up</h1>
+    <div className="OwnerSignupPage">
 
-      <form onSubmit={handleOwnerSignupSubmit}>
+      <div className="min-h-screen py-40" style={{ backgroundImage: "linear-gradient(115deg, rgb(237, 135, 51), rgb(255, 175, 128))" }}>
+        <div className="container mx-auto">
+          <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
 
-        <input type="email" name="ownerEmail" value={ownerEmail} onChange={handleOwnerEmail} placeholder="Enter your email" />
 
-        <input
-          type="password"
-          name="ownerPassword"
-          value={ownerPassword}
-          onChange={handleOwnerPassword} 
-          placeholder="Enter a password"
-        />
+            <div className="w-full lg:w-1/2 flex flex-col items-center  p-12 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${PetImg2})`, objectFit: "scale-down" }}>
+              <h1 style={{ color: "rgb(63, 98, 18)" }} className="text-3xl mb-3 font-bold">Welcome</h1>
+              <div>
+                <p style={{ color: "rgb(63, 98, 18)" }} className="mb-3 font-semibold text-black">If you have a pet available for adoption, then Friends furr-ever app is for you.<br /> <a href="#" className="font-bold" style={{ color: "rgb(63, 98, 18)" }}>Learn more</a></p>
+              </div>
+            </div>
 
-        <input type="text" name="ownerName" value={ownerName} onChange={handleOwnerName} placeholder="Enter your name"/>
+            <div className="w-full lg:w-1/2 py-16 px-12">
+              <h2 className="text-3xl mb-4">Register</h2>
+              <p className="mb-4">
+                Do you want to give up a pet for adoption? <br/> Create your account.
+              </p>
+              <form onSubmit={handleOwnerSignupSubmit}>
 
-        <input type="number" name="ownerPhone" value={ownerPhone} onChange={handleOwnerPhone} placeholder="Enter your phone"/>
+                <div className="grid grid-cols-2 gap-5">
+                  <input className="border border-gray-400 py-1 px-2" type="email" name="ownerEmail" value={ownerEmail} onChange={handleOwnerEmail} placeholder="Enter your email" />
+                  <input className="border border-gray-400 py-1 px-2" type="password" name="ownerPassword" value={ownerPassword} onChange={handleOwnerPassword} placeholder="Enter a password" />
+                </div>
 
-        <input type="text" name="city" value={city} onChange={handleCity} placeholder="City"/>
+                <div className="mt-5"></div>
 
-        <input type="text" name="state" value={state} onChange={handleState} placeholder="State"/>
+                <div className="grid grid-cols-2 gap-5">
+                  <input className="border border-gray-400 py-1 px-2" type="text" name="ownerName" value={ownerName} onChange={handleOwnerName} placeholder="Enter your name" />
+                  <input className="border border-gray-400 py-1 px-2" type="number" name="ownerPhone" value={ownerPhone} onChange={handleOwnerPhone} placeholder="Enter your phone" />
+                </div>
 
-        <input type="number" name="zip" value={zip} onChange={handleZip} placeholder="Zip"/>
+                <div className="mt-5"></div>
 
-        <input type="text" name="country" value={country} onChange={handleCountry} placeholder="Country"/>
+                <div className="grid grid-cols-2 gap-5">
+                  <input className="border border-gray-400 py-1 px-2" type="text" name="city" value={city} onChange={handleCity} placeholder="City" />
+                  <input className="border border-gray-400 py-1 px-2" type="text" name="state" value={state} onChange={handleState} placeholder="State" />
+                </div>
 
-        <button type="submit">Sign Up</button>
-      </form>
+                <div className="mt-5"></div>
 
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
+                <div className="grid grid-cols-2 gap-5">
+                  <input className="border border-gray-400 py-1 px-2" type="number" name="zip" value={zip} onChange={handleZip} placeholder="Zip" />
+                  <input className="border border-gray-400 py-1 px-2" type="text" name="country" value={country} onChange={handleCountry} placeholder="Country" />
+                </div>
 
-      <p>Already have account?</p>
-      <Link to={"/login"}> Login</Link>
+                <div className="mt-5"></div>
+                <button className="w-full py-3 text-center text-white" style={{ backgroundColor: "rgb(63, 98, 18)" }} type="submit">Sign Up</button>
+                
+              </form>
+
+              {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+              <p>Already have account?</p>
+              <Link style={{ color: "rgb(63, 98, 18)" }} to={"/ownerlogin"} className="font-bold" >Login</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
